255476be6b29e1e9bed55406bd31deacb53182a9
nachoCasciaro <icasciaro@est.frba.utn.edu.ar>
2019-10-26 23:54:12 -0300

    arreglando errores script

--- TP/data/script_creación_inicial.sql
+++ TP/data/script_creación_inicial.sql
@@ -1,25 +1,42 @@
 USE [GD2C2019]
 
+--DROP CONTRAINTS
+--Hago un drop de las fk para despues poder hacer un drop de las tablas
+
+DECLARE cursor_tablas CURSOR FOR
+SELECT 
+    'ALTER TABLE [' +  OBJECT_SCHEMA_NAME(parent_object_id) +
+    '].[' + OBJECT_NAME(parent_object_id) + 
+    '] DROP CONSTRAINT [' + name + ']'
+FROM sys.foreign_keys
+
+DECLARE @sql nvarchar(255)
+OPEN cursor_tablas
+FETCH NEXT FROM cursor_tablas INTO @sql
+
+WHILE @@FETCH_STATUS = 0
+	BEGIN
+	exec    sp_executesql @sql
+	FETCH NEXT FROM cursor_tablas INTO @sql
+	END
+CLOSE cursor_tablas
+DEALLOCATE cursor_tablas
 GO
 
---			CREACION SCHEMA
-CREATE SCHEMA POR_COLECTORA AUTHORIZATION gdCupon2019;
-GO
---
 
 --			INICIO DROPEO TABLAS/PROCEDURES/SCHEMA
 
---DROP TABLA DIRECCIONES
-IF OBJECT_ID('POR_COLECTORA.Direcciones', 'U') IS NOT NULL
-DROP TABLE POR_COLECTORA.Direcciones
+--DROP TABLA CLIENTES
+IF OBJECT_ID('POR_COLECTORA.Clientes', 'U') IS NOT NULL
+DROP TABLE POR_COLECTORA.Clientes
 
 --DROP TABLA USUARIOS
 IF OBJECT_ID('POR_COLECTORA.Usuarios', 'U') IS NOT NULL
 DROP TABLE POR_COLECTORA.Usuarios
 
---DROP TABLA CLIENTES
-IF OBJECT_ID('POR_COLECTORA.Clientes', 'U') IS NOT NULL
-DROP TABLE POR_COLECTORA.Clientes
+--DROP TABLA DIRECCIONES
+IF OBJECT_ID('POR_COLECTORA.Direcciones', 'U') IS NOT NULL
+DROP TABLE POR_COLECTORA.Direcciones
 
 --DROP TABLA ROLES
 IF OBJECT_ID('POR_COLECTORA.Roles', 'U') IS NOT NULL
@@ -76,46 +93,53 @@ DROP SCHEMA POR_COLECTORA
 --			FIN DROPEO 
 
 
+GO
+
+--			CREACION SCHEMA
+CREATE SCHEMA POR_COLECTORA AUTHORIZATION gdCupon2019;
+GO
+
+
 --			INICIO CREACION TABLAS
 
 --CREACIÓN DE TABLA DIRECCIONES
 CREATE TABLE POR_COLECTORA.Direcciones(
 	Direccion_Id Numeric IDENTITY(1,1) PRIMARY KEY,
-	Direccion_Calle VARCHAR NOT NULL,
-	Direccion_Nro_Piso VARCHAR,
-	Direccion_Depto VARCHAR,
-	Direccion_Ciudad VARCHAR NOT NULL)
+	Direccion_Calle NVARCHAR(80) NOT NULL,
+	Direccion_Nro_Piso VARCHAR(10),
+	Direccion_Depto VARCHAR(10),
+	Direccion_Ciudad NVARCHAR(80) NOT NULL)
 GO
 
 --CREACIÓN DE TABLA USUARIOS
 CREATE TABLE POR_COLECTORA.Usuarios(
 	Usuario_Id Numeric IDENTITY(1,1) PRIMARY KEY,
-	Usuario_Nombre VARCHAR NOT NULL,
-	Usuario_Password VARCHAR NOT NULL,
-	Usuario_Intentos Numeric DEFAULT 0,
+	Usuario_Nombre VARCHAR(30) NOT NULL,
+	Usuario_Password VARCHAR(30) NOT NULL,
+	Usuario_Intentos Numeric NOT NULL DEFAULT 0,
 	Usuario_Habilitado BIT NOT NULL DEFAULT 1)
 GO
 
 --CREACIÓN DE TABLA CLIENTES
 CREATE TABLE POR_COLECTORA.Clientes(
 	Clie_Id Numeric IDENTITY(1,1) PRIMARY KEY,
-	Clie_Nombre VARCHAR NOT NULL,
-	Clie_Apellido VARCHAR NOT NULL,
+	Clie_Nombre VARCHAR(30) NOT NULL,
+	Clie_Apellido VARCHAR(30) NOT NULL,
 	Clie_DNI Numeric(18,0) NOT NULL UNIQUE,
 	Clie_Mail NVARCHAR(50) NOT NULL,
 	Clie_Telefono Numeric NOT NULL,
-	Clie_Direccion Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Direcciones(Direccion_Id),
-	Clie_CP Numeric NOT NULL,
+	Clie_Direccion Numeric FOREIGN KEY REFERENCES POR_COLECTORA.Direcciones(Direccion_Id),
+	Clie_CP Numeric,
 	Clie_Fecha_Nac DATETIME NOT NULL,
 	Clie_Habilitado BIT NOT NULL DEFAULT 1,
 	Clie_Saldo Numeric NOT NULL DEFAULT 200,
-	Clie_Usuario Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Usuarios(Usuario_Id))
+	Clie_Usuario Numeric FOREIGN KEY REFERENCES POR_COLECTORA.Usuarios(Usuario_Id))
 GO
 
 --CREACIÓN DE TABLA ROLES
 CREATE TABLE POR_COLECTORA.Roles(
 	Rol_Id Numeric IDENTITY(1,1) PRIMARY KEY,
-	Rol_Nombre VARCHAR NOT NULL,
+	Rol_Nombre VARCHAR(30) NOT NULL,
 	Rol_Habilitado BIT NOT NULL DEFAULT 1)
 GO
 
@@ -128,7 +152,7 @@ GO
 --CREACIÓN DE TABLA FUNCIONALIDADES
 CREATE TABLE POR_COLECTORA.Funcionalidades(
 	Func_Id Numeric IDENTITY(1,1) PRIMARY KEY,
-	Func_Descripcion VARCHAR NOT NULL)
+	Func_Descripcion VARCHAR(30) NOT NULL)
 GO
 
 --CREACIÓN DE TABLA FUNCIONALIDADxROL
@@ -140,22 +164,22 @@ GO
 --CREACIÓN DE TABLA RUBROS
 CREATE TABLE POR_COLECTORA.Rubros(
 	Rubro_Id Numeric IDENTITY(1,1) PRIMARY KEY,
-	Rubro_Detalle NVARCHAR NOT NULL)
+	Rubro_Detalle NVARCHAR(50) NOT NULL)
 GO
 
 --CREACIÓN DE TABLA PROVEEDORES
 CREATE TABLE POR_COLECTORA.Proveedores(
 	Provee_Id Numeric IDENTITY(1,1) PRIMARY KEY,
-	Provee_RS VARCHAR NOT NULL,
-	Provee_Mail VARCHAR,
+	Provee_RS NVARCHAR(80) NOT NULL,
+	Provee_Mail NVARCHAR(50),
 	Provee_Telefono Numeric NOT NULL,
-	Provee_CUIT NVARCHAR(13) NOT NULL,
+	Provee_CUIT NVARCHAR(13),
 	Provee_Direccion Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Direcciones(Direccion_Id),
 	Provee_CP Numeric,
 	Provee_Ciudad Numeric NOT NULL,
 	Provee_Rubro Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Rubros(Rubro_Id),
-	Provee_Nombre_Contacto VARCHAR,
-	Provee_Usuario Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Usuarios(Usuario_Id),
+	Provee_Nombre_Contacto NVARCHAR(50),
+	Provee_Usuario Numeric FOREIGN KEY REFERENCES POR_COLECTORA.Usuarios(Usuario_Id),
 	Provee_Habilitado BIT NOT NULL DEFAULT 1)
 GO
 
@@ -168,13 +192,13 @@ CREATE TABLE POR_COLECTORA.Facturas(
 	Fact_Importe Numeric NOT NULL,
 	Fact_Proveedor_ID Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Proveedores(Provee_Id),
 	Fact_Proveedor_CUIT NVARCHAR(13) NOT NULL,
-	Fact_Proveedor_RS VARCHAR NOT NULL)
+	Fact_Proveedor_RS NVARCHAR(80) NOT NULL)
 GO
 
 --CREACIÓN DE TABLA OFERTAS
 CREATE TABLE POR_COLECTORA.Ofertas(
 	Oferta_Id Numeric IDENTITY(1,1) PRIMARY KEY,
-	Oferta_Descripcion VARCHAR NOT NULL,
+	Oferta_Descripcion NVARCHAR(80) NOT NULL,
 	Oferta_Fecha DATETIME NOT NULL,
 	Oferta_Fecha_Venc DATETIME NOT NULL,
 	Oferta_Precio Numeric NOT NULL,
@@ -209,7 +233,7 @@ CREATE TABLE POR_COLECTORA.Tarjetas(
 	Tarjeta_Numero Numeric PRIMARY KEY,
 	Tarjeta_Tipo nvarchar(50) NOT NULL,
 	Tarjeta_Fecha_Venc DATETIME NOT NULL,
-	Cupon_Id_Cliente Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Clientes(Clie_Id))
+	Tarjeta_Id_Cliente Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Clientes(Clie_Id))
 GO
 
 --CREACIÓN DE TABLA CARGAS
@@ -217,32 +241,31 @@ CREATE TABLE POR_COLECTORA.Cargas(
 	Carga_Id Numeric IDENTITY(1,1) PRIMARY KEY,
 	Carga_Fecha DATETIME NOT NULL,
 	Carga_Id_Cliente Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Clientes(Clie_Id),
-	Carga_Tipo_Pago nvarchar(20) NOT NULL, --Es el tipo de tarjeta
+	Carga_Tipo_Pago nvarchar(20), --Es el tipo de tarjeta
 	Carga_Monto NUMERIC NOT NULL,
 	Carga_Id_Tarjeta Numeric NOT NULL FOREIGN KEY REFERENCES POR_COLECTORA.Tarjetas(Tarjeta_Numero),
-	Carga_Medio_Pago VARCHAR(8) NOT NULL)
+	Carga_Medio_Pago VARCHAR(8))
 GO
 
 --			FIN CREACION TABLAS
-
-
 
 --			COMIENZO MIGRACION TABLAS
 
 --MIGRACION DIRECCIONES
 INSERT INTO POR_COLECTORA.Direcciones
-( Direccion_Calle, Direccion_Nro_Piso, Direccion_Depto, Direccion_Ciudad )
-SELECT DISTINCT Cli_Direccion, NULL, NULL, Cli_Ciudad
+( Direccion_Calle,Direccion_Ciudad )
+SELECT DISTINCT Cli_Direccion, Cli_Ciudad
 FROM gd_esquema.Maestra
 -- hacer el insert en direcciones de proveedores
 
+/*
 --MIGRACION USUARIOS
 INSERT INTO POR_COLECTORA.Usuarios
 ( Usuario_Nombre, Usuario_Password )
 SELECT DISTINCT Cli_Nombre, NULL
 FROM gd_esquema.Maestra
 -- hacer el insert en usuarios de proveedores
-
+*/
 --MIGRACION TABLA CLIENTES
 INSERT INTO POR_COLECTORA.Clientes
 (Clie_Nombre, Clie_Apellido, Clie_DNI, Clie_Telefono, Clie_Mail, Clie_CP, Clie_Fecha_Nac, Clie_Direccion, Clie_Usuario)
@@ -253,7 +276,7 @@ FROM gd_esquema.Maestra
 --MIGRACION TABLA ROLES
 INSERT INTO POR_COLECTORA.Roles
 ( Rol_Nombre )
- VALUES ('Proveedor','Cliente','Administrador')
+ VALUES ('Administrador'),('Cliente'),('Proveedor')
 
 --MIGRACION TABLA ROLXUSUARIO
 
@@ -266,27 +289,29 @@ INSERT INTO POR_COLECTORA.Roles
 
 --MIGRACION TABLA PROVEEDORES
 INSERT INTO POR_COLECTORA.Proveedores
-( Provee_RS, Provee_Mail, Provee_Telefono, Provee_CUIT, Provee_Direccion, Provee_CP, Provee_Ciudad, 
-  Provee_Rubro, Provee_Nombre_Contacto, Provee_Usuario)
-SELECT DISTINCT Provee_RS, NULL, Provee_Telefono, Provee_CUIT, 
-				(SELECT Direccion_Id FROM POR_COLECTORA.Direcciones WHERE Direccion_Calle = Provee_Dom), 
-				NULL, Provee_Ciudad, (SELECT Rubro_Id FROM POR_COLECTORA.Rubros WHERE Rubro_Detalle = Provee_Rubro), 
-				NULL, NULL
-FROM gd_esquema.Maestra
+( Provee_RS, Provee_Telefono, Provee_CUIT, Provee_Direccion, Provee_Ciudad, 
+  Provee_Rubro)
+SELECT DISTINCT Maestra.Provee_RS, Maestra.Provee_Telefono, Maestra.Provee_CUIT, 
+				(SELECT Direccion_Id FROM POR_COLECTORA.Direcciones WHERE Direccion_Calle = Maestra.Provee_Dom), 
+				Maestra.Provee_Ciudad, (SELECT Rubro_Id FROM POR_COLECTORA.Rubros WHERE Rubro_Detalle = Maestra.Provee_Rubro)
+FROM gd_esquema.Maestra as Maestra
+
 
 --MIGRACION FACTURAS
 
+/*
 INSERT INTO POR_COLECTORA.Facturas
 	(Fact_Numero,Fact_Fecha_Desde,Fact_Fecha_Hasta,Fact_Importe, Fact_Proveedor_ID,Fact_Proveedor_CUIT,Fact_Proveedor_RS )
-SELECT DISTINCT Factura_Nro,NULL,Factura_Fecha,NULL/*Como saber el importa de la factura? por la oferta?*/,
+SELECT DISTINCT Factura_Nro,NULL,Factura_Fecha,1/*Como saber el importa de la factura? por la oferta?*/,
 	(SELECT Provee_Id FROM POR_COLECTORA.Proveedores As Colectora WHERE Colectora.Provee_RS = Maestra.Provee_RS),
 	(SELECT Provee_CUIT FROM POR_COLECTORA.Proveedores As Colectora WHERE Colectora.Provee_RS = Maestra.Provee_RS),
 	(SELECT Provee_RS FROM POR_COLECTORA.Proveedores As Colectora WHERE Colectora.Provee_RS = Maestra.Provee_RS)
 FROM gd_esquema.Maestra MAESTRA
+*/
 
 --MIGRACION TABLA OFERTAS
 INSERT INTO POR_COLECTORA.Ofertas
-(	Oferta_Descripcion,	Oferta_Fecha, Oferta_Fecha_Venc, Oferta_Precio,	Oferta_Precio_Ficticio,
+(Oferta_Descripcion, Oferta_Fecha, Oferta_Fecha_Venc, Oferta_Precio, Oferta_Precio_Ficticio,
 	Oferta_Cantidad, Oferta_Restriccion_Compra,	Oferta_Proveedor)
 SELECT DISTINCT Oferta_Descripcion, Oferta_Fecha, Oferta_Fecha_Venc, Oferta_Precio, Oferta_Precio_Ficticio, Oferta_Cantidad, 1,
 				(SELECT Provee_Id FROM POR_COLECTORA.Proveedores As Colectora WHERE Colectora.Provee_RS = Maestra.Provee_RS)
