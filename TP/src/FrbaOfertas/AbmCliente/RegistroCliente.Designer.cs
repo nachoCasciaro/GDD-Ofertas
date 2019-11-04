b144f3d96d74c48abd8f793395610da7a0a995ea
Fiamma Coedo <coedo.fiamma@gmail.com>
2019-11-04 19:33:21 -0300

    filtro proveedor y pantallas inicio abm

--- TP/src/FrbaOfertas/AbmCliente/RegistroCliente.Designer.cs
+++ TP/src/FrbaOfertas/AbmCliente/RegistroCliente.Designer.cs
@@ -61,219 +61,246 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 27);
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 73);
+            this.label2.Location = new System.Drawing.Point(28, 59);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(132, 27);
+            this.textBox1.Location = new System.Drawing.Point(99, 19);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(113, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(132, 73);
+            this.textBox2.Location = new System.Drawing.Point(99, 56);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.Size = new System.Drawing.Size(113, 20);
             this.textBox2.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 164);
+            this.label3.Location = new System.Drawing.Point(28, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.Size = new System.Drawing.Size(44, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Nombre";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(129, 164);
+            this.textBox3.Location = new System.Drawing.Point(97, 130);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.Size = new System.Drawing.Size(115, 20);
             this.textBox3.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(40, 216);
+            this.label4.Location = new System.Drawing.Point(30, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.Size = new System.Drawing.Size(44, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Apellido";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(129, 216);
+            this.textBox4.Location = new System.Drawing.Point(97, 173);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.Size = new System.Drawing.Size(115, 20);
             this.textBox4.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(40, 274);
+            this.label5.Location = new System.Drawing.Point(30, 223);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(31, 17);
+            this.label5.Size = new System.Drawing.Size(26, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = "DNI";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(129, 269);
+            this.textBox5.Location = new System.Drawing.Point(97, 220);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.Size = new System.Drawing.Size(115, 20);
             this.textBox5.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(40, 318);
+            this.label6.Location = new System.Drawing.Point(30, 258);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(33, 17);
+            this.label6.Size = new System.Drawing.Size(26, 13);
             this.label6.TabIndex = 10;
             this.label6.Text = "Mail";
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(129, 318);
+            this.textBox6.Location = new System.Drawing.Point(97, 255);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.Size = new System.Drawing.Size(115, 20);
             this.textBox6.TabIndex = 11;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(40, 366);
+            this.label7.Location = new System.Drawing.Point(30, 297);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(64, 17);
+            this.label7.Size = new System.Drawing.Size(49, 13);
             this.label7.TabIndex = 12;
             this.label7.Text = "Teléfono";
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(129, 366);
+            this.textBox7.Location = new System.Drawing.Point(97, 294);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.Size = new System.Drawing.Size(115, 20);
             this.textBox7.TabIndex = 13;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(391, 27);
+            this.label8.Location = new System.Drawing.Point(293, 22);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(39, 17);
+            this.label8.Size = new System.Drawing.Size(30, 13);
             this.label8.TabIndex = 14;
             this.label8.Text = "Calle";
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(463, 27);
+            this.textBox8.Location = new System.Drawing.Point(358, 19);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(268, 22);
+            this.textBox8.Size = new System.Drawing.Size(191, 20);
             this.textBox8.TabIndex = 15;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(391, 76);
+            this.label9.Location = new System.Drawing.Point(293, 62);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(62, 17);
+            this.label9.Size = new System.Drawing.Size(47, 13);
             this.label9.TabIndex = 16;
             this.label9.Text = "Nro Piso";
             this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(463, 76);
+            this.textBox9.Location = new System.Drawing.Point(358, 59);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(76, 22);
+            this.textBox9.Size = new System.Drawing.Size(58, 20);
             this.textBox9.TabIndex = 17;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(574, 76);
+            this.label10.Location = new System.Drawing.Point(430, 62);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(46, 17);
+            this.label10.Size = new System.Drawing.Size(36, 13);
             this.label10.TabIndex = 18;
             this.label10.Text = "Depto";
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(631, 77);
+            this.textBox10.Location = new System.Drawing.Point(473, 59);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(76, 22);
+            this.textBox10.Size = new System.Drawing.Size(76, 20);
             this.textBox10.TabIndex = 19;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(391, 136);
+            this.label11.Location = new System.Drawing.Point(293, 110);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(52, 17);
+            this.label11.Size = new System.Drawing.Size(40, 13);
             this.label11.TabIndex = 20;
             this.label11.Text = "Ciudad";
             // 
             // textBox11
             // 
-            this.textBox11.Location = new System.Drawing.Point(463, 136);
+            this.textBox11.Location = new System.Drawing.Point(358, 107);
+            this.textBox11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(173, 22);
+            this.textBox11.Size = new System.Drawing.Size(191, 20);
             this.textBox11.TabIndex = 21;
             // 
             // textBox12
             // 
-            this.textBox12.Location = new System.Drawing.Point(508, 190);
+            this.textBox12.Location = new System.Drawing.Point(417, 151);
+            this.textBox12.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox12.Name = "textBox12";
-            this.textBox12.Size = new System.Drawing.Size(100, 22);
+            this.textBox12.Size = new System.Drawing.Size(132, 20);
             this.textBox12.TabIndex = 22;
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(391, 190);
+            this.label12.Location = new System.Drawing.Point(293, 154);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(95, 17);
+            this.label12.Size = new System.Drawing.Size(72, 13);
             this.label12.TabIndex = 23;
             this.label12.Text = "Código Postal";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(391, 241);
+            this.label13.Location = new System.Drawing.Point(293, 196);
+            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(141, 17);
+            this.label13.Size = new System.Drawing.Size(108, 13);
             this.label13.TabIndex = 24;
             this.label13.Text = "Fecha de Nacimiento";
             // 
             // textBox13
             // 
-            this.textBox13.Location = new System.Drawing.Point(556, 241);
+            this.textBox13.Location = new System.Drawing.Point(417, 193);
+            this.textBox13.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox13.Name = "textBox13";
-            this.textBox13.Size = new System.Drawing.Size(100, 22);
+            this.textBox13.Size = new System.Drawing.Size(132, 20);
             this.textBox13.TabIndex = 25;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(435, 312);
+            this.button1.Location = new System.Drawing.Point(326, 254);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(201, 23);
+            this.button1.Size = new System.Drawing.Size(151, 19);
             this.button1.TabIndex = 26;
             this.button1.Text = "REGISTRARME";
             this.button1.UseVisualStyleBackColor = true;
@@ -281,9 +308,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(435, 363);
+            this.button2.Location = new System.Drawing.Point(326, 295);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(201, 23);
+            this.button2.Size = new System.Drawing.Size(151, 19);
             this.button2.TabIndex = 27;
             this.button2.Text = "VOLVER AL MENU LOGIN";
             this.button2.UseVisualStyleBackColor = true;
@@ -291,9 +319,9 @@
             // 
             // RegistroCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 451);
+            this.ClientSize = new System.Drawing.Size(602, 366);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox13);
@@ -322,6 +350,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RegistroCliente";
             this.Text = "RegistroCliente";
             this.Load += new System.EventHandler(this.RegistroCliente_Load);
---
+++ TP/src/FrbaOfertas/AbmProveedor/FiltroBMProveedor.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace FrbaOfertas.AbmProveedor
+{
+    partial class FiltroBMProveedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbox_rs = new System.Windows.Forms.TextBox();
+            this.txtbox_cuit = new System.Windows.Forms.TextBox();
+            this.txtbox_mail = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FILTRAR = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.razonsocial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rubro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filtrar por:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Razón Social";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CUIT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mail";
+            // 
+            // txtbox_rs
+            // 
+            this.txtbox_rs.Location = new System.Drawing.Point(273, 76);
+            this.txtbox_rs.Name = "txtbox_rs";
+            this.txtbox_rs.Size = new System.Drawing.Size(236, 20);
+            this.txtbox_rs.TabIndex = 4;
+            // 
+            // txtbox_cuit
+            // 
+            this.txtbox_cuit.Location = new System.Drawing.Point(273, 113);
+            this.txtbox_cuit.Name = "txtbox_cuit";
+            this.txtbox_cuit.Size = new System.Drawing.Size(236, 20);
+            this.txtbox_cuit.TabIndex = 5;
+            // 
+            // txtbox_mail
+            // 
+            this.txtbox_mail.Location = new System.Drawing.Point(273, 150);
+            this.txtbox_mail.Name = "txtbox_mail";
+            this.txtbox_mail.Size = new System.Drawing.Size(236, 20);
+            this.txtbox_mail.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.razonsocial,
+            this.mail,
+            this.telefono,
+            this.rubro});
+            this.dataGridView1.Location = new System.Drawing.Point(93, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(562, 156);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // FILTRAR
+            // 
+            this.FILTRAR.Location = new System.Drawing.Point(633, 106);
+            this.FILTRAR.Name = "FILTRAR";
+            this.FILTRAR.Size = new System.Drawing.Size(75, 23);
+            this.FILTRAR.TabIndex = 8;
+            this.FILTRAR.Text = "FILTRAR";
+            this.FILTRAR.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "BAJA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(439, 375);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "MODIFICAR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // razonsocial
+            // 
+            this.razonsocial.HeaderText = "Razon Social";
+            this.razonsocial.Name = "razonsocial";
+            // 
+            // mail
+            // 
+            this.mail.HeaderText = "Mail";
+            this.mail.Name = "mail";
+            // 
+            // telefono
+            // 
+            this.telefono.HeaderText = "Teléfono";
+            this.telefono.Name = "telefono";
+            // 
+            // rubro
+            // 
+            this.rubro.HeaderText = "Rubro";
+            this.rubro.Name = "rubro";
+            // 
+            // FiltroBMProveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 410);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.FILTRAR);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtbox_mail);
+            this.Controls.Add(this.txtbox_cuit);
+            this.Controls.Add(this.txtbox_rs);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FiltroBMProveedor";
+            this.Text = "FiltroBMProveedor";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbox_rs;
+        private System.Windows.Forms.TextBox txtbox_cuit;
+        private System.Windows.Forms.TextBox txtbox_mail;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button FILTRAR;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn razonsocial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rubro;
+    }
+}
