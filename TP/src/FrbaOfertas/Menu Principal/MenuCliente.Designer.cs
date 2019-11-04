e1e88c9ec77263a33ebc0ca0f4b48eecd3e05fcb
nachoCasciaro <icasciaro@est.frba.utn.edu.ar>
2019-11-03 22:49:20 -0300

    menu cliente

---
+++ TP/src/FrbaOfertas/Menu Principal/MenuCliente.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FrbaOfertas.Menu_Principal
+{
+    partial class MenuCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "COMPRAR OFERTA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(45, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 102);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CARGAR CRÉDITO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(131, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MENU";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MenuCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 308);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MenuCliente";
+            this.Text = "MenuCliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ TP/src/FrbaOfertas/Menu Principal/MenuCliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaOfertas.Menu_Principal
+{
+    public partial class MenuCliente : Form
+    {
+        public MenuCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new 
+        }
+    }
+}
