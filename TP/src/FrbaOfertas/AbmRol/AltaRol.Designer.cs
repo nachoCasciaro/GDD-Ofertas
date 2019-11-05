ded5a7b0c367a9e6f230e46ae89ec7b7d9f4bc25
Fiamma Coedo <coedo.fiamma@gmail.com>
2019-11-05 14:02:44 -0300

    interfaz alta rol

---
+++ TP/src/FrbaOfertas/AbmRol/AltaRol.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FrbaOfertas.AbmRol
+{
+    partial class AltaRol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbox_nombrerol = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datos nuevo rol:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Funcionalidades";
+            // 
+            // txtbox_nombrerol
+            // 
+            this.txtbox_nombrerol.Location = new System.Drawing.Point(254, 62);
+            this.txtbox_nombrerol.Name = "txtbox_nombrerol";
+            this.txtbox_nombrerol.Size = new System.Drawing.Size(220, 20);
+            this.txtbox_nombrerol.TabIndex = 3;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Login y Seguridad",
+            "ABM Rol",
+            "Registro de Usuario",
+            "ABM Cliente",
+            "ABM Proveedor",
+            "Cargar Crédito",
+            "Comprar Oferta",
+            "Confección y Publicación de Ofertas",
+            "Entrega/Consumo de Oferta",
+            "Facturación a Proveedor",
+            "Listado Estadístico"});
+            this.checkedListBox1.Location = new System.Drawing.Point(254, 107);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(220, 184);
+            this.checkedListBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CREAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AltaRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 401);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.txtbox_nombrerol);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AltaRol";
+            this.Text = "AltaRol";
+            this.Load += new System.EventHandler(this.AltaRol_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbox_nombrerol;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ TP/src/FrbaOfertas/AbmRol/AltaRol.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace FrbaOfertas.AbmRol
+{
+    public partial class AltaRol : Form
+    {
+        public AltaRol()
+        {
+            InitializeComponent();
+        }
+
+        private void AltaRol_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void validarDatos()
+        {
+            if (Validaciones.estaVacio(txtbox_nombrerol.Text))
+            {
+
+                throw new Exception("Debe completar todos los campos");
+
+            }
+
+        }
+
+        private void crearRol()
+        {
+            var connection = DB.getInstance().getConnection();
+            SqlCommand query = new SqlCommand("POR_COLECTORA.sp_alta_rol", connection);
+            query.CommandType = CommandType.StoredProcedure;
+
+            query.Parameters.Add(new SqlParameter("@nombre", this.txtbox_nombrerol.Text));
+            //Que onda los valores de los check box (?)
+
+            connection.Open();
+            query.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.validarDatos();
+                this.crearRol();
+                this.Close();
+            }
+            catch (Exception excepcion)
+            {
+                MessageBox.Show(excepcion.Message, "Error", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
